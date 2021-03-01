4bb62c476e0893bfc94558b1734f8b549d9bca62
Long Nguyen <elongmusk39@gmail.com>
2021-02-28 21:23:06 -0800

    Initial Commit

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Long Nguyen on 2/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
